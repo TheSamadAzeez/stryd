8068bc6dc473507965746e45d69422237fe3192c
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-12-27 19:49:38 +0100

    Implement ClerkProvider for authentication and handle missing publishable key error

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,24 @@
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
+import { Slot } from 'expo-router';
 import '../global.css';
+import { StatusBar } from 'react-native';
 
-import { Stack } from 'expo-router';
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env.local file'
+  );
+}
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+      <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+        <Slot />
+      </ClerkProvider>
+    </>
+  );
 }
