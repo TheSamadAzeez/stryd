1a8e64d1ca53bf1258e95293f48dc7536a1b258f
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-12-29 08:59:45 +0100

    feat: enhance tab bar styling and icon configuration in layout

--- app/(app)/(main)/_layout.tsx
+++ app/(app)/(main)/_layout.tsx
@@ -1,9 +1,26 @@
+import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 
 export default function Layout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ headerShown: false }} />
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: '#f8fafc', // slate-50 equivalent
+        },
+        tabBarActiveTintColor: '#1A1A1A',
+        tabBarInactiveTintColor: '#9CA3AF',
+        // tabBarActiveBackgroundColor: '#f8fafc',
+        // tabBarInactiveBackgroundColor: '#f8fafc',
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerShown: false,
+          tabBarIcon: () => <Ionicons name="play" size={30} color="#1A1A1A" />,
+          tabBarShowLabel: false,
+        }}
+      />
     </Tabs>
   );
 }
